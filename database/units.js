8e7c02dd12e7faf597a5b234ffebc3019be41803
Kawaki1963 <kawaki12353@gmail.com>
2026-05-14 18:06:56 -0300

    Atualizar o units.js

--- database/units.js
+++ database/units.js
@@ -2,5 +2,25 @@ const units = {
     "Shinobi Sage (Friendship)": {
         trait: "monarch",
         memoria: "Trust of Others"
+    },
+    "Agony Shinobi (Force)": {
+        trait: "monarch",
+        memoria: "Trust of Others"
+    },
+    "Shinobi God (Infinite Dreams)": {
+        trait: "monarch",
+        memoria: "Trust of Others"
+    },
+    "Fastest Shinobi (Flash)": {
+        trait: "monarch",
+        memoria: "Trust of Others"
+    },
+    "Alocard (Vampire King)": {
+        trait: "monarch",
+        memoria: "Trust of Others"
+    },
+    "Lich King (Ruler)": {
+        trait: "monarch",
+        memoria: "Trust of Others"
     }
 };
