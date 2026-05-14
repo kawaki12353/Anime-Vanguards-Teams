82ef6b2b495cbb79fac2d00d279479bb84498ffa
Kawaki1963 <kawaki12353@gmail.com>
2026-05-14 18:01:37 -0300

    Criar teams.js

---
+++ database/teams.js
@@ -0,0 +1,10 @@
+const units = {
+    "Ninjutsu Team": {
+        slot1: "Shinobi Sage (Friendship)",
+        slot2: "Agony Shinobi (Force)",
+        slot3: "Shinobi God (Infinite Dreams)",
+        slot4: "Fastest Shinobi (Flash)",
+        slot5: "Alocard (Vampire King)",
+        slot6: "Lich King (Ruler)"
+    }
+};
