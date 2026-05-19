92598987c819f69bd48a1a1dcf429529b962c586
Kawaki1963 <kawaki12353@gmail.com>
2026-05-19 17:44:52 -0300

    Atualizar o units.js

--- database/units.js
+++ database/units.js
@@ -1,50 +1,60 @@
 const units = {
     "Strongest Shinobi (Martial)": {
+        elements: "Nature",
         trait: "Monarch",
         image: "Strongest_Shinobi_(Martial).png",
         memoria: "All I've Got"
     },
     "Manipulator (Spider)": {
+        elements: "Spark",
         trait: "Blitz",
         image: "Manipulator_(Spider).png",
         memoria: "Ultimate Deceiver"
     },
     "Hebano (Clematis)": {
+        elements: "Fire",
         trait: "Deadeye",
         image: "Hebano_(Clematis).png",
         memoria: "DoT Amplifier"
     },
     "Senator (Nanomachines)": {
+        elements: "Fire",
         trait: "Deadeye",
         image: "Senator_(Nanomachines).png",
         memoria: "Company Captain's Presence"
     },
     "Shinobi Sage (Friendship)": {
+        elements: ["Nature", "Spark"],
         trait: "Monarch",
         image: "Shinobi_Sage.png",
         memoria: "Trust of Others"
     },
     "Agony Shinobi (Force)": {
+        elements: "Holy",
         trait: "Monarch",
         image: "Agony_Shinobi_(Force).png",
         memoria: "Shall Know Pain"
     },
     "Shinobi God (Infinite Dreams)": {
+        elements: ["Nature", "Fire"],
         trait: "Monarch",
         image: "Shinobi_God_(Infinite_Dreams).png",
         memoria: "Peak of Shinobi"
     },
     "Fastest Shinobi (Flash)": {
+        elements: ["Nature", "Spark"],
         trait: "Monarch",
         image: "Fastest_Shinobi_(Flash).png",
         memoria: "A Father's Sacrifice"
     },
     "Alocard (Vampire King)": {
+        elements: "Unbound",
         trait: "Monarch",
         image: "Alocard_(Vampire_King).png",
         memoria: "Enthusiastic Walks"
     },
     "Lich King (Ruler)": {
+        elements: "Curse",
         trait: "Monarch",
         image: "Lich_King_(Ruler).png",
         memoria: "Enthusiastic Walks"
