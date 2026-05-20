d7ab768cf252b3f99a299c3b3fbb4e303beb087c
Kawaki1963 <kawaki12353@gmail.com>
2026-05-20 12:39:00 -0300

    Atualizar o units.js

--- database/units.js
+++ database/units.js
@@ -3,60 +3,70 @@ const units = {
         elements: "Nature",
         trait: "[1]Monarch[1]",
         image: "Strongest_Shinobi_(Martial).png",
+        familiar: "Birb",
         memoria: "All I've Got"
     },
     "Manipulator (Spider)": {
         elements: "Spark",
         trait: "[2]Blitz[2]",
         image: "Manipulator_(Spider).png",
+        familiar: "The Doctor",
         memoria: "Ultimate Deceiver"
     },
     "Hebano (Clematis)": {
         elements: "Fire",
         trait: "[1]Deadeye[1]",
         image: "Hebano_(Clematis).png",
+        familiar: "Birb",
         memoria: "DoT Amplifier"
     },
     "Senator (Nanomachines)": {
         elements: "Fire",
         trait: "[1]Deadeye[1]",
         image: "Senator_(Nanomachines).png",
+        familiar: "Birb",
         memoria: "Company Captain's Presence"
     },
     "Shinobi Sage (Friendship)": {
         elements: ["Nature", "Spark"],
         trait: "[1]Monarch[1]",
         image: "Shinobi_Sage.png",
+        familiar: "Birb",
         memoria: "Trust of Others"
     },
     "Agony Shinobi (Force)": {
         elements: "Holy",
         trait: "[1]Monarch[1]",
         image: "Agony_Shinobi_(Force).png",
+        familiar: "Nose",
         memoria: "Shall Know Pain"
     },
     "Shinobi God (Infinite Dreams)": {
         elements: ["Nature", "Fire"],
         trait: "[1]Monarch[1]",
         image: "Shinobi_God_(Infinite_Dreams).png",
+        familiar: "Birb",
         memoria: "Peak of Shinobi"
     },
     "Fastest Shinobi (Flash)": {
         elements: ["Nature", "Spark"],
         trait: "[1]Monarch[1]",
         image: "Fastest_Shinobi_(Flash).png",
+        familiar: "Birb",
         memoria: "A Father's Sacrifice"
     },
     "Alocard (Vampire King)": {
         elements: "Unbound",
         trait: "[1]Monarch[1]",
         image: "Alocard_(Vampire_King).png",
+        familiar: "Vampire's Seal",
         memoria: "Enthusiastic Walks"
     },
     "Lich King (Ruler)": {
         elements: "Curse",
         trait: "[1]Monarch[1]",
         image: "Lich_King_(Ruler).png",
+        familiar: "The Doctor",
         memoria: "Enthusiastic Walks"
     }
 };
