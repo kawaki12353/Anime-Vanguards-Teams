0239cdd8bddccc20c4bf2f1488a62d30142dac0f
Kawaki1963 <kawaki12353@gmail.com>
2026-05-20 18:37:18 -0300

    Atualizar o teams.js

--- database/teams.js
+++ database/teams.js
@@ -1,4 +1,15 @@
 const teams = {
+    "DoT Team": {
+        dps: "18mi",
+        tags: ["Tower"],
+        slot1: "Lich King (Ruler)",
+        slot2: "Alocard (Vampire King)",
+        slot3: "Demon Hybrid (Chainsaw)",
+        slot4: "Hebano (Clematis)",
+        slot5: "Manipulator (Spider)",
+        slot6: "Armored Mage (Requip)",
+        caloric: "Delusional Demon (Blood)"
+    },
     "Burn Team": {
         dps: "23mi",
         tags: ["Tower"],
@@ -17,6 +28,7 @@ const teams = {
         slot3: "Shinobi God (Infinite Dreams)",
         slot4: "Fastest Shinobi (Flash)",
         slot5: "Alocard (Vampire King)",
-        slot6: "Lich King (Ruler)"
+        slot6: "Lich King (Ruler)",
+        caloric: "Savior (Moonless)"
     }
 };
