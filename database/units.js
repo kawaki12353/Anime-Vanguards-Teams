a9518de0be23e3d1b67868ee8a58fd3cccf6e117
Kawaki1963 <kawaki12353@gmail.com>
2026-05-20 12:02:06 -0300

    Atualizar o units.js

--- database/units.js
+++ database/units.js
@@ -1,61 +1,61 @@
 const units = {
     "Strongest Shinobi (Martial)": {
         elements: "Nature",
-        trait: "Monarch",
+        trait: "[1]Monarch[1]",
         image: "Strongest_Shinobi_(Martial).png",
         memoria: "All I've Got"
     },
     "Manipulator (Spider)": {
         elements: "Spark",
-        trait: "Blitz",
+        trait: "[2]Blitz[2]",
         image: "Manipulator_(Spider).png",
         memoria: "Ultimate Deceiver"
     },
     "Hebano (Clematis)": {
         elements: "Fire",
-        trait: "Deadeye",
+        trait: "[1]Deadeye[1]",
         image: "Hebano_(Clematis).png",
         memoria: "DoT Amplifier"
     },
     "Senator (Nanomachines)": {
         elements: "Fire",
-        trait: "Deadeye",
+        trait: "[1]Deadeye[1]",
         image: "Senator_(Nanomachines).png",
         memoria: "Company Captain's Presence"
     },
     "Shinobi Sage (Friendship)": {
         elements: ["Nature", "Spark"],
-        trait: "Monarch",
+        trait: "[1]Monarch[1]",
         image: "Shinobi_Sage.png",
         memoria: "Trust of Others"
     },
     "Agony Shinobi (Force)": {
         elements: "Holy",
-        trait: "Monarch",
+        trait: "[1]Monarch[1]",
         image: "Agony_Shinobi_(Force).png",
         memoria: "Shall Know Pain"
     },
     "Shinobi God (Infinite Dreams)": {
         elements: ["Nature", "Fire"],
-        trait: "Monarch",
+        trait: "[1]Monarch[1]",
         image: "Shinobi_God_(Infinite_Dreams).png",
         memoria: "Peak of Shinobi"
     },
     "Fastest Shinobi (Flash)": {
         elements: ["Nature", "Spark"],
-        trait: "Monarch",
+        trait: "[1]Monarch[1]",
         image: "Fastest_Shinobi_(Flash).png",
         memoria: "A Father's Sacrifice"
     },
     "Alocard (Vampire King)": {
         elements: "Unbound",
-        trait: "Monarch",
+        trait: "[1]Monarch[1]",
         image: "Alocard_(Vampire_King).png",
         memoria: "Enthusiastic Walks"
     },
     "Lich King (Ruler)": {
         elements: "Curse",
-        trait: "Monarch",
+        trait: "[1]Monarch[1]",
         image: "Lich_King_(Ruler).png",
         memoria: "Enthusiastic Walks"
     }
