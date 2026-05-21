d4c883074377c202e3947c792625d22fcdcc7dca
Kawaki1963 <kawaki12353@gmail.com>
2026-05-21 15:30:41 -0300

    Atualizar o teams.js

--- database/teams.js
+++ database/teams.js
@@ -2,12 +2,12 @@ const teams = {
     "DoT Team": {
         dps: "18mi",
         tags: ["Tower"],
-        slot1: {unit: "Lich King (Ruler)", m_trait: "Divinity"},
+        slot1: {unit: "Lich King (Ruler)", m_trait: "Jack of All"},
         slot2: {unit: "Alocard (Vampire King)", m_trait: "Divinity"},
         slot3: {unit: "Demon Hybrid (Chainsaw)", m_trait: "Divinity"},
-        slot4: {unit: "Hebano (Clematis)", m_trait: "Divinity"},
-        slot5: {unit: "Manipulator (Spider)", m_trait: "Divinity"},
-        slot6: {unit: "Armored Mage (Requip)", m_trait: "Divinity"},
+        slot4: {unit: "Hebano (Clematis)", m_trait: "Contagious"},
+        slot5: {unit: "Manipulator (Spider)", m_trait: "Contagious"},
+        slot6: {unit: "Armored Mage (Requip)", m_trait: "Contagious"},
         caloric: {unit: "Delusional Demon (Blood)", m_trait: "Divinity"}
     },
     "Burn Team": {
@@ -16,9 +16,9 @@ const teams = {
         slot1: {unit: "Strongest Shinobi (Martial)", m_trait: "Divinity"},
         slot2: {unit: "Agony Shinobi (Force)", m_trait: "Divinity"},
         slot3: {unit: "Alocard (Vampire King)", m_trait: "Divinity"},
-        slot4: {unit: "Hebano (Clematis)", m_trait: "Divinity"},
-        slot5: {unit: "Manipulator (Spider)", m_trait: "Divinity"},
-        slot6: {unit: "Senator (Nanomachines)", m_trait: "Divinity"}
+        slot4: {unit: "Hebano (Clematis)", m_trait: "Contagious"},
+        slot5: {unit: "Manipulator (Spider)", m_trait: "Contagious"},
+        slot6: {unit: "Senator (Nanomachines)", m_trait: "Contagious"}
     },
     "Ninjutsu Team": {
         dps: "24mi",
@@ -28,7 +28,7 @@ const teams = {
         slot3: {unit: "Shinobi God (Infinite Dreams)", m_trait: "Divinity"},
         slot4: {unit: "Fastest Shinobi (Flash)", m_trait: "Divinity"},
         slot5: {unit: "Alocard (Vampire King)", m_trait: "Divinity"},
-        slot6: {unit: "Lich King (Ruler)", m_trait: "Divinity"},
+        slot6: {unit: "Lich King (Ruler)", m_trait: "Jack of All"},
         caloric: {unit: "Savior (Moonless)", m_trait: "Divinity"}
     }
 };
