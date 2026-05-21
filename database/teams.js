324f085c75cde06bf04459309243b67e09579110
Kawaki1963 <kawaki12353@gmail.com>
2026-05-21 10:53:21 -0300

    Atualizar o teams.js

--- database/teams.js
+++ database/teams.js
@@ -2,33 +2,33 @@ const teams = {
     "DoT Team": {
         dps: "18mi",
         tags: ["Tower"],
-        slot1: "Lich King (Ruler)",
-        slot2: "Alocard (Vampire King)",
-        slot3: "Demon Hybrid (Chainsaw)",
-        slot4: "Hebano (Clematis)",
-        slot5: "Manipulator (Spider)",
-        slot6: "Armored Mage (Requip)",
-        caloric: "Delusional Demon (Blood)"
+        slot1: {unit: "Lich King (Ruler)", m_trait: "Divinity"},
+        slot2: {unit: "Alocard (Vampire King)", m_trait: "Divinity"},
+        slot3: {unit: "Demon Hybrid (Chainsaw)", m_trait: "Divinity"},
+        slot4: {unit: "Hebano (Clematis)", m_trait: "Divinity"},
+        slot5: {unit: "Manipulator (Spider)", m_trait: "Divinity"},
+        slot6: {unit: "Armored Mage (Requip)", m_trait: "Divinity"},
+        caloric: {unit: "Delusional Demon (Blood)", m_trait: "Divinity"}
     },
     "Burn Team": {
         dps: "23mi",
         tags: ["Tower"],
-        slot1: "Strongest Shinobi (Martial)",
-        slot2: "Agony Shinobi (Force)",
-        slot3: "Alocard (Vampire King)",
-        slot4: "Hebano (Clematis)",
-        slot5: "Manipulator (Spider)",
-        slot6: "Senator (Nanomachines)"
+        slot1: {unit: "Strongest Shinobi (Martial)", m_trait: "Divinity"},
+        slot2: {unit: "Agony Shinobi (Force)", m_trait: "Divinity"},
+        slot3: {unit: "Alocard (Vampire King)", m_trait: "Divinity"},
+        slot4: {unit: "Hebano (Clematis)", m_trait: "Divinity"},
+        slot5: {unit: "Manipulator (Spider)", m_trait: "Divinity"},
+        slot6: {unit: "Senator (Nanomachines)", m_trait: "Divinity"}
     },
     "Ninjutsu Team": {
         dps: "24mi",
         tags: ["Tower", "Progressive"],
-        slot1: "Shinobi Sage (Friendship)",
-        slot2: "Agony Shinobi (Force)",
-        slot3: "Shinobi God (Infinite Dreams)",
-        slot4: "Fastest Shinobi (Flash)",
-        slot5: "Alocard (Vampire King)",
-        slot6: "Lich King (Ruler)",
-        caloric: "Savior (Moonless)"
+        slot1: {unit: "Shinobi Sage (Friendship)", m_trait: "Divinity"},
+        slot2: {unit: "Agony Shinobi (Force)", m_trait: "Divinity"},
+        slot3: {unit: "Shinobi God (Infinite Dreams)", m_trait: "Divinity"},
+        slot4: {unit: "Fastest Shinobi (Flash)", m_trait: "Divinity"},
+        slot5: {unit: "Alocard (Vampire King)", m_trait: "Divinity"},
+        slot6: {unit: "Lich King (Ruler)", m_trait: "Divinity"},
+        caloric: {unit: "Savior (Moonless)", m_trait: "Divinity"}
     }
 };
